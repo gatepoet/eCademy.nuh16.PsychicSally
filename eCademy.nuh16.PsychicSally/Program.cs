994ee5912907ae08b0ca68ed1251a0a8a308da38
kristoffer.deinoff <kristoffer.deinoff@gmail.com>
2016-11-09 19:58:49 +0100

    Nettsamling 3 Basic input parsing

--- eCademy.nuh16.PsychicSally/Program.cs
+++ eCademy.nuh16.PsychicSally/Program.cs
@@ -10,6 +10,35 @@ namespace eCademy.nuh16.PsychicSally
     {
         static void Main(string[] args)
         {
+            var maxTries = 5;
+            var random = new Random();
+            var numberToGuess = random.Next(0, 100);
+
+            for (var i=0; i < maxTries; i++)
+            {
+                Console.Write("Skriv inn et tall mellom 1 og 100: ");
+                var input = Console.ReadLine();
+                int guessedNumber;
+                if (int.TryParse(input, out guessedNumber))
+                {
+                    if (guessedNumber > numberToGuess)
+                    {
+                        Console.WriteLine("Feil. Du gjettet for høyt");
+                    }
+                    else if (guessedNumber < numberToGuess)
+                    {
+                        Console.WriteLine("Feil. Du gjettet for lavt");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Gratulerer!");
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine("Trykk en tast for å avslutte");
+            Console.ReadKey();
         }
     }
 }
