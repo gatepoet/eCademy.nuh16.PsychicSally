d9c1fe9b93d92f999ba2e6921e83c9547de78c0b
kristoffer.deinoff <kristoffer.deinoff@gmail.com>
2016-11-23 20:20:57 +0100

    Added possibility to specify max tries as an argument

--- eCademy.nuh16.PsychicSally/Program.cs
+++ eCademy.nuh16.PsychicSally/Program.cs
@@ -8,37 +8,59 @@ namespace eCademy.nuh16.PsychicSally
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             var maxTries = 5;
+            foreach (var argument in args)
+            { 
+                var argumentParts = argument.Split('=');
+                if (argument.ToLower().StartsWith("-maxtries"))
+                {
+                    var argumentName = argumentParts[0];
+                    var argumentValue = argumentParts[1];
+                    maxTries = int.Parse(argumentValue);
+                }
+            }
             var random = new Random();
             var numberToGuess = random.Next(0, 100);
 
             for (var i=0; i < maxTries; i++)
             {
-                Console.Write("Skriv inn et tall mellom 1 og 100: ");
-                var input = Console.ReadLine();
-                int guessedNumber;
-                if (int.TryParse(input, out guessedNumber))
+                int guessedNumber = ReadNumber();
+
+                if (guessedNumber > numberToGuess)
                 {
-                    if (guessedNumber > numberToGuess)
-                    {
-                        Console.WriteLine("Feil. Du gjettet for høyt");
-                    }
-                    else if (guessedNumber < numberToGuess)
-                    {
-                        Console.WriteLine("Feil. Du gjettet for lavt");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Gratulerer!");
-                        break;
-                    }
+                    Console.WriteLine("Du gjettet for høyt");
+                }
+                else if (guessedNumber < numberToGuess)
+                {
+                    Console.WriteLine("Du gjettet for lavt");
+                }
+                else
+                {
+                    Console.WriteLine("Gratulerer!");
+                    break;
                 }
             }
 
             Console.WriteLine("Trykk en tast for å avslutte");
             Console.ReadKey();
+        }
+
+        private static int ReadNumber()
+        {
+            Console.Write("Skriv inn et tall mellom 1 og 100: ");
+            var input = Console.ReadLine();
+            int guessedNumber;
+            if (int.TryParse(input, out guessedNumber))
+            {
+                return guessedNumber;
+            }
+            else
+            {
+                Console.WriteLine("Du må skrive inn et gyldig heltall!");
+                return ReadNumber();
+            }
         }
     }
 }
